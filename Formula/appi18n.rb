d1b583f9eb0ba610d380a2b58d104182f4873226
小弟调调 <398188662@qq.com>
2026-02-26 22:23:23 +0800

    feat: Update appi18n formula URL and SHA256 checksum to version 1.0.2

--- Formula/appi18n.rb
+++ Formula/appi18n.rb
@@ -3,8 +3,8 @@
 class Appi18n < Formula
   desc "Lightweight CLI tool for unifying and optimizing the internationalization (i18n) workflow across multiple apps."
   homepage "https://github.com/jaywcjlove/app-i18n"
-  url "https://github.com/jaywcjlove/app-i18n/releases/download/v1.0.1/appi18n.tar.gz"
-  sha256 "037d4bbceaa44c43493e04b97ced2c0bf76b417717db0b5d3226662a61da315f"
+  url "https://github.com/jaywcjlove/app-i18n/releases/download/v1.0.2/appi18n.tar.gz"
+  sha256 "d9bd74a5f892202f8f0593d7fa99e2b23b2388536b06fae8d0e873d3f6b8c99c"
   license "MIT"
 
   def install
