ef73dc1ce61823ec1c6db3cd5a21261e48310f0d
小弟调调 <398188662@qq.com>
2026-02-26 21:34:02 +0800

    Add appi18n formula

---
+++ Formula/app-i18n.rb
@@ -0,0 +1,38 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class AppI18n < Formula
+  desc "Lightweight CLI tool for unifying and optimizing the internationalization (i18n) workflow across multiple apps."
+  homepage "https://github.com/jaywcjlove/app-i18n"
+  url "https://github.com/jaywcjlove/app-i18n/releases/download/v1.0.0/appi18n.tar.gz"
+  sha256 "037d4bbceaa44c43493e04b97ced2c0bf76b417717db0b5d3226662a61da315f"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test app-i18n`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
