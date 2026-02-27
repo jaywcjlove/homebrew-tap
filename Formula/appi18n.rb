7c0bac64319bd046f07046958a33a8c415c4181d
小弟调调 <398188662@qq.com>
2026-02-27 22:07:17 +0800

    released v1.1.1

--- Formula/appi18n.rb
+++ Formula/appi18n.rb
@@ -3,8 +3,8 @@
 class Appi18n < Formula
   desc "Lightweight CLI tool for unifying and optimizing the internationalization (i18n) workflow across multiple apps."
   homepage "https://github.com/jaywcjlove/app-i18n"
-  url "https://github.com/jaywcjlove/app-i18n/releases/download/v1.1.0/appi18n.tar.gz"
-  sha256 "c36bcdec04ca201f782b8ea5496320d9945b890e5a542f6de234b813923ef76c"
+  url "https://github.com/jaywcjlove/app-i18n/releases/download/v1.1.1/appi18n.tar.gz"
+  sha256 "bff0f394b8a506a12d879b43731d6a4398293ff5fa20edaa3977be86ea287a3a"
   license "MIT"
 
   def install
