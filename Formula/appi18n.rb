5af496f3ef67e84d474a3a79742741019ee65f61
小弟调调 <398188662@qq.com>
2026-04-07 22:07:05 +0800

    chore(appi18n): released v1.4.0

--- Formula/appi18n.rb
+++ Formula/appi18n.rb
@@ -3,8 +3,8 @@
 class Appi18n < Formula
   desc "Lightweight CLI tool for unifying and optimizing the internationalization (i18n) workflow across multiple apps."
   homepage "https://github.com/jaywcjlove/app-i18n"
-  url "https://github.com/jaywcjlove/app-i18n/releases/download/v1.3.0/appi18n.tar.gz"
-  sha256 "d86278620e931d7dc7fcd2945242a5d7cc06a200a1321b10c9b0c1013b1c7000"
+  url "https://github.com/jaywcjlove/app-i18n/releases/download/v1.4.0/appi18n.tar.gz"
+  sha256 "bec985e368ec58921ba338fff284e5a4d64b902d4b763a42419afb9ee61f9663"
   license "MIT"
 
   def install
