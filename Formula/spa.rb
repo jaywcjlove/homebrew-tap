226cf55788abc59e31f1d46fdc32fa4559b81b15
小弟调调 <398188662@qq.com>
2026-06-11 05:15:36 +0800

    released v0.1.5 (spa)

--- Formula/spa.rb
+++ Formula/spa.rb
@@ -3,8 +3,8 @@
 class Spa < Formula
   desc "Swift Package Add for Xcode GUI projects"
   homepage "https://github.com/jaywcjlove/spa"
-  url "https://github.com/jaywcjlove/spa/releases/download/v0.1.4/spa.tar.gz"
-  sha256 "aa006026c2a9260634565ed4d386cf4bee72f1b23a39006ab292335e64baf3b1"
+  url "https://github.com/jaywcjlove/spa/releases/download/v0.1.5/spa.tar.gz"
+  sha256 "f26c92fb5bfd650b6c2fc9005c7523424c203f0a46cde9328d7e82294e1e5d3c"
   license "MIT"
 
   def install
